32b5821f37861892776fd73527c30f3756bce7a5
critical-node <yiiiiii2024@gmail.com>
2024-09-09 11:43:31 +0800

    fix: 修改联系页面样式

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,81 +4,80 @@ import IconGithub from "@/components/SvgIcon/Github";
 
 export default function Page() {
   return (
-    <div>
-      <div className="mb-10">
-        <div>title</div>
-        <div>subtitle</div>
-      </div>
-      <div className="flex flex-row justify-between items-start gap-20">
-        <div className="w-full flex flex-col items-start">
-          <div className="text-5xl font-bold mb-5">Y18i</div>
-          <div className="text-lg mb-5">y18i - Next.js Enginee</div>
+    <div className="flex flex-row justify-between items-start gap-24 mt-20">
+      <div className="mb-5 gap-y-8 w-full flex flex-col items-start">
+        <div className="mb-10">
+          <div className="text-2xl font-medium mb-5">Contact 联系</div>
           <div className="mb-5">
-            <div>
-              <IconWechat className="w-8 h-8" />
-              WeChat
-            </div>
-            <div>
-              <IconEmail className="w-8 h-8" /> <div>Email</div>
-            </div>
-            <div>
-              <IconGithub className="w-8 h-8" /> <div>Github</div>
-            </div>
+            You can contact me through the following contact information or
+            leave your message
           </div>
+          <div className="">可以通过以下的联系方式联系我，或者留下你的信息</div>
         </div>
-        <div className="w-full flex flex-col items-start  ">
-          <div className="text-xl font-bold mb-5">或者留下一个信息</div>
-          <form
-            className="w-full max-w-lg"
-            name="contact"
-            method="post"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
+        <div className="flex justify-center items-center gap-x-5">
+          <IconEmail className="w-6 h-6" />
+          <div className="font-medium">yiiiii2020@163.com</div>
+        </div>
+        <div className="flex justify-center items-center gap-x-5">
+          <IconGithub className="w-6 h-6" />
+          <div className="font-medium">yiiiii2020@163.com</div>
+        </div>
+        <div className="flex justify-center items-center gap-x-5">
+          <IconWechat className="w-6 h-6" />
+          <div className="font-medium">Wechat:ZY18i</div>
+        </div>
+      </div>
+      <div className="w-full flex flex-col items-start  ">
+        <form
+          className="w-full max-w-lg"
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="bot-field" />
+          <div className="flex flex-col mb-5">
+            <label className="text-lg font-medium mb-2" htmlFor="name">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="border-2 border-gray-300 rounded-md px-4 py-2 w-full"
+              required
+            />
+          </div>
+          <div className="flex flex-col mb-5">
+            <label className="text-lg font-medium  mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="border-2 border-gray-300 rounded-md px-4 py-2 w-full"
+              required
+            />
+          </div>
+          <div className="flex flex-col mb-5">
+            <label className="text-lg font-medium  mb-2" htmlFor="message">
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className="border-2 border-gray-300 rounded-md px-4 py-2 w-full h-40"
+              required
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="w-full text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
           >
-            <input type="hidden" name="bot-field" />
-            <div className="flex flex-col mb-5">
-              <label className="text-lg" htmlFor="name">
-                Name
-              </label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                className="border-2 border-gray-300 rounded-md px-4 py-2 w-full"
-                required
-              />
-            </div>
-            <div className="flex flex-col mb-5">
-              <label className="text-lg" htmlFor="email">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="border-2 border-gray-300 rounded-md px-4 py-2 w-full"
-                required
-              />
-            </div>
-            <div className="flex flex-col mb-5">
-              <label className="text-lg" htmlFor="message">
-                Message
-              </label>
-              <textarea
-                name="message"
-                id="message"
-                className="border-2 border-gray-300 rounded-md px-4 py-2 w-full h-40"
-                required
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="w-full text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
-            >
-              Send
-            </button>
-          </form>
-        </div>
+            Send
+          </button>
+        </form>
       </div>
     </div>
   );
