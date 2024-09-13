4ceeed530b32c83e4dae6af7718e4224ab217829
Y18i <13187886985@163.com>
2024-09-13 15:38:12 +0800

    fix: 中间代码

--- src/components/SvgIcon/Search.tsx
+++ src/components/SvgIcon/Search.tsx
@@ -1,20 +1,13 @@
 export default function Search(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
       viewBox="0 0 24 24"
+      fill="currentColor"
       height="1em"
       width="1em"
       {...props}
     >
-      <path stroke="none" d="M0 0h24v24H0z" />
-      <path d="M19 11V8a2 2 0 00-2-2H5a2 2 0 00-2 2v5a2 2 0 002 2h5" />
-      <path d="M18 15.5 A2.5 2.5 0 0 1 15.5 18 A2.5 2.5 0 0 1 13 15.5 A2.5 2.5 0 0 1 18 15.5 z" />
-      <path d="M17.5 17.5L20 20" />
+      <path d="M15.5 12c2.5 0 4.5 2 4.5 4.5 0 .88-.25 1.71-.69 2.4l3.08 3.1L21 23.39l-3.12-3.07c-.69.43-1.51.68-2.38.68-2.5 0-4.5-2-4.5-4.5s2-4.5 4.5-4.5m0 2a2.5 2.5 0 00-2.5 2.5 2.5 2.5 0 002.5 2.5 2.5 2.5 0 002.5-2.5 2.5 2.5 0 00-2.5-2.5M5 3h14c1.11 0 2 .89 2 2v8.03c-.5-.8-1.19-1.49-2-2.03V5H5v14h4.5c.31.75.76 1.42 1.31 2H5c-1.11 0-2-.89-2-2V5c0-1.11.89-2 2-2m2 4h10v2H7V7m0 4h5.03c-.8.5-1.49 1.19-2.03 2H7v-2m0 4h2.17c-.11.5-.17 1-.17 1.5v.5H7v-2z" />
     </svg>
   );
 }
