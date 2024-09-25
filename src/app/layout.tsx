61f2521e21514d0f78eb92721a431d994344bd4a
Y18i <13187886985@163.com>
2024-09-25 18:11:11 +0800

    fix: 更新代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import Navigation from "@/components/Navigation";
 import BackToTop from "@/components/BackToTop";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
+// import { Inter } from "next/font/google";
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
